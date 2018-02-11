c7145ef2ba43e56f63a849d56b0afa8b55ad5999
Wagner Sodré <wsodre@br.ibm.com>
2018-02-11 17:54:52 -0200

    some changes were done in express server

--- main.js
+++ main.js
@@ -72,7 +72,13 @@ const handleMessage = ({entities}) => {
 const client = new Wit({accessToken});
 interactive(client, handleMessage);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+    res.send('hello world');
 });
+
+// Only works on 3000 regardless of what I set environment port to or how I set
+// [value] in app.set('port', [value]).
+// app.listen(3000);
+app.listen(app.get('port'));
