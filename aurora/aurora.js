4d2d8fac49e1c0d0b7aad7a1e0e41ab20ecfb26d
Wagner Sodré <wsodre@br.ibm.com>
2018-02-12 13:32:39 -0200

    Created new routes and homepage

--- aurora/aurora.js
+++ aurora/aurora.js
@@ -3,12 +3,7 @@ exports.talk = function(msg) {
 
   let Wit = null;
   let interactive = null;
-  try {
-    // if running from repo
-    Wit = require('../').Wit;
-  } catch (e) {
-    Wit = require('node-wit').Wit;
-  }
+  Wit = require('node-wit').Wit;
 
   const accessToken = '5QGZLIXIE5TKWBTHH3KOWPBF5YSBAJBP';
 
@@ -78,6 +73,85 @@ let answer;
       } else {
         answer = (JSON.stringify(rsp));
       }
+    }).then(() => {
+      return answer;
+    }).catch(function (err) {
+      return console.error(err);
+    }));
+
+};
+
+exports.json = function(msg) {
+  'use strict';
+
+  let Wit = null;
+  let interactive = null;
+  Wit = require('node-wit').Wit;
+
+  const accessToken = '5QGZLIXIE5TKWBTHH3KOWPBF5YSBAJBP';
+
+  // Joke example
+  // See https://wit.ai/aforaleka/wit-example-joke-bot/
+
+  const allJokes = {
+    chuck: [
+      'Chuck Norris counted to infinity - twice.',
+      'Death once had a near-Chuck Norris experience.',
+    ],
+    tech: [
+      'Did you hear about the two antennas that got married? The ceremony was long and boring, but the reception was great!',
+      'Why do geeks mistake Halloween and Christmas? Because Oct 31 === Dec 25.',
+    ],
+    default: [
+      'Why was the Math book sad? Because it had so many problems.',
+    ],
+  };
+
+  const firstEntityValue = (entities, entity) => {
+    const val = entities && entities[entity] &&
+      Array.isArray(entities[entity]) &&
+      entities[entity].length > 0 &&
+      entities[entity][0].value
+    ;
+    if (!val) {
+      return null;
+    }
+    return typeof val === 'object' ? val.value : val;
+  };
+
+  const handleMessage = ({entities}) => {
+    const getJoke = firstEntityValue(entities, 'getJoke');
+    const greetings = firstEntityValue(entities, 'greetings');
+    const bye = firstEntityValue(entities, 'bye');
+    const category = firstEntityValue(entities, 'category');
+    const sentiment = firstEntityValue(entities, 'sentiment');
+    // if (getJoke) {
+    //   if (category) {
+    //     const jokes = allJokes[category];
+    //     return(jokes[Math.floor(Math.random() * jokes.length)]);
+    //   } else {
+    //     return(allJokes['default'][0]);
+    //   }
+    // } else if (sentiment) {
+    //   const reply = sentiment === 'positive' ? 'Glad you liked it.' : 'Hmm.';
+    //   return(reply);
+    // }
+    if (bye) {
+      return("See ya!");
+    } else if (greetings) {
+      return("Hi, my name is Aurora, how are you?");
+    } else {
+      return("I can tell jokes! Say 'tell me a joke about tech'!");
+    }
+  };
+
+  const client = new Wit({accessToken});
+  //interactive(client, handleMessage);
+let answer;
+
+  return(  client.message(msg)
+    .then(function (rsp) {
+        answer = (JSON.stringify(rsp));
     }).then(() => {
       return answer;
     }).catch(function (err) {
--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var aurora = require("./aurora.js");
+var aurora = require("./aurora/aurora.js");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/console', function(req, res){
+  res.sendFile(__dirname + '/console/index.html');
+});
+
+app.route('/answer/:msg')
+  .get(function(req, res, msg) {
+    aurora.talk(req.params.msg).then((rsp)=>{res.send(rsp);});
+  });
+
+app.route('/json/:msg')
+  .get(function(req, res, msg) {
+    aurora.json(req.params.msg).then((rsp)=>{res.send(rsp);});
+  });
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -16,8 +30,6 @@ io.on('connection', function(socket){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
     aurora.talk(msg).then((rsp)=>{io.emit('chat message',rsp);});
-    //console.log('aurora: ' + aurora.talk(msg));
-    //io.emit(aurora.talk(msg));
   });
 });
 
