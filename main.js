a0899945f6b81135f3b4e02fe68079d0e7c77559
Wagner Sodré <wsodre@br.ibm.com>
2018-02-12 16:27:51 -0200

    Editing Home Page

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var aurora = require("./aurora/aurora.js");
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
